4ef289d8fe1b51535fbeb40f98d1a87435e148b7
Jerry Tong <zihangtong@gmail.com>
2021-11-24 17:40:52 -0500

    commit tree 2a7cc129670d5e73889498953220fc1eb724637c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// function superBowlWin(record) {
+//     let gameWin = record.year;
+//     if (record.result === "W") {
+//         return gameWin.find();
+//     } else {
+//         return undefined;
+//     }
+// }
 
+
+
+
+const superbowlWin = (record) => {
+    // iterate over the record
+    const firstWin = record.find(season => {
+        // for each object (element) return the first instance where they key of results equals "W"
+        return season.result === "W"
+
+        // need to return a BOOLEAN
+    })
+    console.log(firstWin)
+    return !!firstWin ? firstWin.year : firstWin
+}
